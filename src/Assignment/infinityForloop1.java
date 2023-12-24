1296194e45e68ed141a37fd890ff001b1395a107
JARVIS <JARVIS@DESKTOP-DUJG4GQ>
2023-12-24 15:40:42 +0530

    updates

---
+++ src/Assignment/infinityForloop1.java
@@ -0,0 +1,12 @@
+package Assignment;
+
+public class infinityForloop1 {
+	public static void main(String[] args) {
+		for (;;) {
+			// Code inside the loop
+			System.out.println("This is an infinite for loop!");
+		}
+
+	}
+
+}
---
+++ src/Assignment/infinityForloop2.java
@@ -0,0 +1,13 @@
+package Assignment;
+
+public class infinityForloop2 {
+	 public static void main(String[] args) {
+	        boolean alwaysTrue = true;
+	        for (; alwaysTrue; ) {
+	            // Code inside the loop
+	            System.out.println("This is another way to create an infinite for loop!");
+	        }
+	        // Code after the infinite loop 
+	    }
+
+}
