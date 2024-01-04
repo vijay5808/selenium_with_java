c56215cc5a51d865c30d537b9d8a52859f8d7ab9
JARVIS <JARVIS@DESKTOP-DUJG4GQ>
2024-01-04 08:06:10 +0530

    updates

---
+++ src/Assignment/SingleLevel_Inheritence.java
@@ -0,0 +1,18 @@
+package Assignment;
+
+public class SingleLevel_Inheritence {
+
+}
+
+class Parents{
+	public void
+	Behaviour() {
+		
+	}
+}
+
+class Child extends Parents{
+	public void callmethod() {
+		Behaviour();
+	}
+}
